5f3f8342a3a923b9ed0b291f6856c15428bb1eb7
Yonerfy <yonerfy15@gmail.com>
2025-03-17 16:25:44 +0100

    initial project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+
+
+
+
+</script>
+
+<template>
+  <h1>Meet landing page</h1>
+
+  <div class="card">
+    
+  </div>
+
+  
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
